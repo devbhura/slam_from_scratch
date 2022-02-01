8b13b60211769db38f9a2d2cfd7083015b7d137e
devbhura <devbhura@gmail.com>
2022-02-01 00:01:58 -0600

    merge master

--- turtlelib/include/turtlelib/diff_drive.hpp
+++ turtlelib/include/turtlelib/diff_drive.hpp
@@ -2,8 +2,45 @@
 #define DIFF_DRIVE_INCLUDE_GUARD_HPP
 
 #include <math.h> 
+#include "rigid2d.hpp"
 /// \file
 /// \brief 
+
+namespace turtlelib
+{
+
+    struct WheelPhi
+    {
+        double left_phi=0.0;
+        double right_phi=0.0;
+
+    };
+
+    class DiffDrive
+    {
+        private:
+            double d=0.0;
+            double r=0.0;
+
+            WheelPhi phi;
+
+            Transform2D T;
+
+
+        public:
+
+            DiffDrive();
+
+            explicit DiffDrive(double dist, double radius);
+
+            explicit DiffDrive(double dist, double radius, Transform2D transform);
+
+            Twist getTwist(Vector2D u);
+
+            Vector2D getwheelvel()
+
+    };
+}
 
 
 
